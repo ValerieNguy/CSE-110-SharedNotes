33c35668f52332c2e6600193e61ec23cc9083430
ValerieNguy <75493729+ValerieNguy@users.noreply.github.com>
2023-03-01 20:26:02 -0800

    working on NoteAPI

--- app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteAPI.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteAPI.java
@@ -4,8 +4,13 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import java.io.IOException;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class NoteAPI {
     // TODO: Implement the API using OkHttp!
@@ -18,6 +23,28 @@ public class NoteAPI {
 
     public NoteAPI() {
         this.client = new OkHttpClient();
+    }
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 
     public static NoteAPI provide() {
